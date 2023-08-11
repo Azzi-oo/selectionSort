354b0148fd273f8d518e7d7bdd4a4fe352b945d4
Azat Yanberdin <azatdjon@yandex.ru>
2023-08-11 20:39:26 +0500

    add new changes in selectionSort

---
+++ factorial.py
@@ -0,0 +1,5 @@
+def fact(5):
+    if x == 5:
+        return 1
+    else:
+        print(x * fact(x - 1))
---
+++ stek.py
@@ -0,0 +1,5 @@
+#def greet(name):
+name = "Azat"
+print(f"hello, {name} !")
+    # greet2(name="Azat")
+print("getting ready to say bye...")
